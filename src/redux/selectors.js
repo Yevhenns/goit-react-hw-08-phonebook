874dfa8acc07215661e7884aa5ddb5537ff7e71e
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-08 10:32:34 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,3 +2,7 @@ export const getItems = state => state.contacts.items;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
 export const getFilter = state => state.filter;
+
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+export const getUserName = state => state.auth.user.name;
+export const getIsLoadingLogin = state => state.auth.isLoading;
