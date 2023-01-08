61222d0c99f06f96b0b4ae408e2bc9e8198545fd
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-08 10:13:54 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Container } from '@mui/material';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>      
-        <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <Container maxWidth="sm">
+            <App />
+          </Container>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
