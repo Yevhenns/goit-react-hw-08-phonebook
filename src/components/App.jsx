b376d745d6a8521b5f31720100559d597781ba7e
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-09 21:24:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,61 @@
-import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Contacts } from 'pages/Contacts';
+import { Registration } from 'pages/Registration';
+import { NotFound } from 'pages/NotFound';
+import { Login } from 'pages/Login';
+import { Navigation } from './Navigation/Navigation';
+import { GlobalStyle } from 'components/GlobalStyles';
+import Box from '@mui/material/Box';
+import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { Form } from './Form/Form';
-import { Contacts } from './Contacts/Contacts';
-import { Filter } from './Filter/Filter';
-import { useSelector, useDispatch } from 'react-redux';
-import { getItems, getIsLoading, getError } from 'redux/selectors';
-import { fetchContacts } from 'redux/contacts/contactsApi';
+import { fetchCurrentUser } from 'redux/auth/authApi';
+import { PrivateRoute } from '../Routes/PrivateRoute';
+import { RestrictedRoute } from '../Routes/RestrictedRoute';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import css from './App.module.css';
 
 export const App = () => {
-  const array = useSelector(getItems);
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <Form />
-      <h2>Contacts</h2>
-      {array.length === 0 ? (
-        <p>No contacts to show</p>
-      ) : (
-        <>
-          <Filter />
-          {isLoading && !error && <p>Loading contacts...</p>}
-          <Contacts />
-        </>
-      )}
-    </div>
+    <>
+      <Box sx={{ width: '100%' }}>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route index element={<Navigate to="/login" />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<Registration />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute redirectTo="/login" component={<Contacts />} />
+              }
+            />
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Box>
+      <GlobalStyle />
+      <ToastContainer />
+    </>
   );
 };
