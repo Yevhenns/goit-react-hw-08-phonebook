1bd75d6ff0f5d8189392f06da8b149678677c6b7
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-01-22 08:59:09 +0200

    Delete RestrictedRoute.js

--- src/Routes/RestrictedRoute.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux';
-import { getIsLoggedIn } from 'redux/selectors';
-import { Navigate } from 'react-router-dom';
-
-export const RestrictedRoute = ({ component: Component, redirectTo }) => {
-  const isLoggedIn = useSelector(getIsLoggedIn);
-  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-};
